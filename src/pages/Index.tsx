58937990f941cc224702d3d8616b5c0c2dbfaf13
abxdigitallfounder-web <abxdigitall.founder@gmail.com>
2026-03-11 14:28:18 -0300

    Add files via upload

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
-import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
-import DashboardSection from "@/components/DashboardSection";
+import PressSection from "@/components/PressSection";
+import MediaMarquee from "@/components/MediaMarquee";
 import CalculadoraSection from "@/components/CalculadoraSection";
 import MapaLastroSection from "@/components/MapaLastroSection";
 import LogosSection from "@/components/LogosSection";
@@ -44,9 +44,9 @@ const Index = () => {
 
   return (
     <div className="min-h-screen" style={{ background: "var(--bg-deep)" }}>
-      <Navbar />
       <HeroSection />
-      <DashboardSection />
+      <MediaMarquee />
+      <PressSection />
       <hr className="hr" />
       <CalculadoraSection />
       <hr className="hr" />
